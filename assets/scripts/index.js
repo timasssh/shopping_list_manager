44112004de48cb2be9b9929f79b7ec4aa0d20bc7
timas <timmylbueno@gmail.com>
2026-01-16 15:57:44 -0300

    refactor: now the function that shows the menu is diferent from the function that switch between menus + fix: the menu was being initialized wrong

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,25 +1,29 @@
 const switchMenuButtons = document.querySelectorAll(".mainContentOption");
 const menus = document.querySelectorAll(".menu");
 
+function showMenu(menuToShow = 0) {
+    if(!menuToShow) {
+        switchMenuButtons[0].classList.add("selectedOption");
+        switchMenuButtons[1].classList.remove("selectedOption");
+
+        menus[0].classList.remove("hidden");
+        menus[1].classList.add("hidden")
+    }else {
+        switchMenuButtons[1].classList.add("selectedOption");
+        switchMenuButtons[0].classList.remove("selectedOption");
+
+        menus[1].classList.remove("hidden");
+        menus[0].classList.add("hidden")
+    }
+}
 function switchMenu(index) {
     let activeMenu = switchMenuButtons[0].classList.contains("selectedOption") ? 0 : 1;
     if(index !== activeMenu) {
-        if(activeMenu) {
-            switchMenuButtons[0].classList.add("selectedOption");
-            switchMenuButtons[1].classList.remove("selectedOption");
-
-            menus[0].classList.add("hidden");
-            menus[1].classList.remove("hidden")
-        }else {
-            switchMenuButtons[1].classList.add("selectedOption");
-            switchMenuButtons[0].classList.remove("selectedOption");
-
-            menus[1].classList.add("hidden");
-            menus[0].classList.remove("hidden")
-        }
+        showMenu(!activeMenu);
     }
 }
 
+showMenu();
 switchMenuButtons.forEach((element, index) => {
     element.addEventListener("click", () => switchMenu(index));
 });
