de92dd1e5a0e727c865afa37eb628502fe70619c
timas <timmylbueno@gmail.com>
2026-01-20 16:17:55 -0300

    feat: now the 'add product to list form', when submited, creates a new product (with createProduct.js), then adds it on the products list (with addProductOnList.js); the index.js imports the functions from other files and just control the events of the page and which function needs to be executed

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,23 @@
 import { showMenu, switchMenu } from "./menu_scripts.js";
+import { createProduct } from "./createProduct.js";
+import { addProductOnList } from "./addProductOnList.js";
 
 const switchMenuButtons = document.querySelectorAll(".mainContentOption");
+const form = document.querySelector("#addProductOnListForm");
+const productsList = [];
+const productListElement = document.getElementById("productsList");
 
 showMenu();
 switchMenuButtons.forEach((element, index) => {
     element.addEventListener("click", () => switchMenu(index));
+});
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let newProduct = createProduct(form);
+    productsList.push(newProduct);
+    console.log(productsList);
+
+    addProductOnList(productListElement, newProduct);
 });
