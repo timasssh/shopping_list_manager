516bfad972aada8dce5f89486f74b9c0235f33ef
timas <timmylbueno@gmail.com>
2026-01-21 19:32:07 -0300

    refactor: refactoring how 'menu_scripts.js' access the HTML tags: now they're imported from the 'index.js' file ensuring independence and maintainability

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,7 @@
 import { showMenu, switchMenu } from "./menu_scripts.js";
 
-const switchMenuButtons = document.querySelectorAll(".mainContentOption");
+export const switchMenuButtons = document.querySelectorAll(".mainContentOption");
+export const menus = document.querySelectorAll(".menu");
 
 showMenu();
 switchMenuButtons.forEach((element, index) => {
